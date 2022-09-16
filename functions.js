9b45ca4baf0ad70cb9e02a69ce03a6ad8ec54386
McSpencer076 <merijn2@gmail.com>
2022-09-16 06:04:01 +0200

    Added colorpicker. Set basic lay-out.

--- functions.js
+++ functions.js
@@ -1,74 +1,87 @@
-const container = document.querySelector('#container');
+const sketchGrid = document.querySelector('#sketchGrid');
 const gridButton = document.querySelector("#changeGrid");
-const slider = document.querySelector('#slider')
-const randomColorBtn = document.querySelector("#randomColorBtn");
+const slider = document.querySelector('#slider');
+const randomColorBtn = document.querySelector("#randomColorBtn")
+const colorPicker = document.querySelector("#colorPicker")
 
-let screenVal = document.querySelector('.value');
+let screenVal = document.querySelector('#gridValue');
 let val = document.getElementById('slider').value;
-let randomColor = randomColorValue();
-let grid = createGrid();
+screenVal.textContent = "Gridsize: " + val;
 
-screenVal.textContent = val;
-
-function createGrid(){
+function createGrid() {
     resetGrid();
+    let screenVal = document.querySelector('#gridValue');
     let val = document.getElementById('slider').value;
-    screenVal.textContent = val;
-    container.setAttribute("style", `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`)
-    for (i=1; i<= val*val; i++){
-            const divGrid = document.createElement("div");
-            divGrid.classList.add("divGrid");
-            divGrid.id = "divGrid";
-            container.appendChild(divGrid);
-            divGrid.addEventListener("mouseover", function(divGrid){
-                divGrid.target.style.backgroundColor = "black";
-            })
+    sketchGrid.setAttribute("style", `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`)
+    for (i = 1; i <= val * val; i++) {
+        const divGrid = document.createElement("div");
+        divGrid.classList.add("divGrid");
+        divGrid.id = "divGrid";
+        sketchGrid.appendChild(divGrid);
+        divGrid.addEventListener("mouseover", function (divGrid) {
+            divGrid.target.style.backgroundColor = "black";
+        })
     }
 }
-
-function createGridRandomColor(){
-    resetGrid();
-    let val = document.getElementById('slider').value;
-    screenVal.textContent = val;
-    container.setAttribute("style", `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`)
-    for (i=1; i<= val*val; i++){
-            const divGrid = document.createElement("div");
-            divGrid.classList.add("divGrid");
-            divGrid.id = "divGrid";
-            container.appendChild(divGrid);
-            divGrid.addEventListener("mouseover", function(divGrid){
-                divGrid.target.style.backgroundColor = randomColorValue();
-            })
-    }
+function setBlackColor() {
+    const divGrid = document.querySelectorAll("div.divGrid");
+    divGrid.forEach(divGrid => {
+        divGrid.addEventListener("mouseover", function (divGrid) {
+            divGrid.target.style.backgroundColor = "black";
+        })
+    })
 }
 
-function resetGrid(){
+function setRandomColor() {
+    const divGrid = document.querySelectorAll("div.divGrid");
+    divGrid.forEach(divGrid => {
+        divGrid.addEventListener("mouseover", function(divGrid) {
+            divGrid.target.style.backgroundColor = randomColorValue();
+        })
+    })
+}
+
+function gridColorPicker(){
+    const divGrid = document.querySelectorAll("div.divGrid");
+    divGrid.forEach(divGrid => {
+        divGrid.addEventListener("mouseover", function(divGrid) {
+            divGrid.target.style.backgroundColor = hexToRGB();
+        })
+    })
+}
+
+function resetGrid() {
     const resetGrid = document.querySelectorAll(".divGrid")
     resetGrid.forEach(divGrid => {
         divGrid.remove();
     });
 }
 
-function randomColorValue(){
-    function randomInteger(max){
-        return Math.floor(Math.random()*(max + 1));
+function randomColorValue() {
+    function randomInteger(max) {
+        return Math.floor(Math.random() * (max + 1));
     }
     let r = randomInteger(255);
     let g = randomInteger(255);
     let b = randomInteger(255);
-    let colorValues = "rgb(" + r + ', ' + g + ', ' + b + ")"
-    return colorValues
+    let colorValue = `rgb(${r},${g},${b})`;
+    return colorValue
 }
 
-container.classList.add("container");
+function hexToRGB(){
+    let colorHEX = document.getElementById("colorPicker").value;
+        let r = parseInt(colorHEX.substring(1, 3), 16);
+        let g = parseInt(colorHEX.substring(3, 5), 16);
+        let b = parseInt(colorHEX.substring(5, 7), 16);
+    let colorRGB = `rgb(${r},${g},${b})`
+    return colorRGB;
+}
 
-gridButton.addEventListener("click", createGrid);
-randomColorBtn.addEventListener("click", createGridRandomColor);
+sketchGrid.classList.add("sketchGrid");
 
-slider.addEventListener("input", function(){
-    resetGrid();
-    let val = document.getElementById('slider').value;
-    screenVal.textContent = val;
-})
+slider.addEventListener("input", createGrid)
+gridButton.addEventListener("click", setBlackColor);
+randomColorBtn.addEventListener("click", setRandomColor);
+colorPicker.addEventListener("change", gridColorPicker);
 
 createGrid();
